22f97e969c76eba2da402827faf2063bf7079d7e
YananMao <YananMao@users.noreply.github.com>
2026-03-20 11:35:14 +0800

    fix: 修复 Cloudflare Pages 部署流程

---
+++ open-next.config.ts
@@ -0,0 +1,7 @@
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+
+const config = defineCloudflareConfig({
+  // Cloudflare specific overrides can be added here
+});
+
+export default config;
